ee60cd78a1012425ae0a7a65ebaa7431cda18f98
nilay-amd <nilay11412@gmail.com>
2026-06-20 21:57:47 +0530

    new

---
+++ bintodec.py
@@ -0,0 +1,11 @@
+binary = input("Enter a binary number: ")
+
+decimal = 0
+power = 0
+
+for digit in reversed(binary):
+    if digit == '1':
+        decimal += 2 ** power
+    power += 1
+
+print("Decimal representation is:", decimal)
